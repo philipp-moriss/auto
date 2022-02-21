5d98a51f05ed9a3c64f07a28ccb8a087963d1c26
philipp-moriss <filverem@gmail.com>
2022-02-21 16:00:39 +0300

    update main branch

--- components/PriceCard/PriceCard.tsx
+++ components/PriceCard/PriceCard.tsx
@@ -7,13 +7,16 @@ export const PriceCard: React.FC<PriceCardPropsType> = ({price, Title}) => {
     return (
         <>
             <Card
+                bordered={false}
+                bodyStyle={{backgroundColor:'rgba(0,0,0,0.62)'}}
                 className={style.PriceCard}
                 hoverable
                 style={{width: 240}}
                 cover={<img alt="example"
                             src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&fit=crop"/>}
             >
-                <Meta title={Title} description={price}/>
+                <Meta title={Title} style={{padding: '20px'}}/>
+                <span style={{color: '#f5a906'}}>цена : {price}</span>
             </Card>
         </>
     )
